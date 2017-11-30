347d4468d98b55ac58810335a60edf913f521df4
cice <cice@192.168.3.121>
2017-11-30 21:07:05 +0100

    Inicio proyecto

---
+++ src/coom/cice/documentacion/JavaDoc.java
@@ -0,0 +1,26 @@
+
+package coom.cice.documentacion;
+
+/**
+ * Clase JavaDoc. Desde aqui se inicieará la ejecución del programa
+ * @author cice
+ */
+public class JavaDoc {
+
+    public static void main(String... args) {
+        /*
+        A) int a = 0, b = 0, c = 0;
+        B) String nombre, String apellidos;
+        C) int a, b, c = 0;
+        D) Float d;
+        E) double a, b, c;
+        
+        solo funcionarian la A y D si ponemos todas estas variables en el mimos codigo
+        
+        */
+        
+        
+        
+    }
+    
+}
